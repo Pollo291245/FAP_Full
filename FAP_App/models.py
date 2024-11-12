d38747aa4fa19089aed3f1e0e638f9ff933535f9
Laboratorio Computacion Talca <tal_lab@inacap.cl>
2024-11-12 10:34:44 -0300

    actualizacion

--- FAP_App/models.py
+++ FAP_App/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.utils import timezone
 
 class UserProfile(models.Model):
     ROLE_CHOICES = [
@@ -91,10 +92,11 @@ class AdoptionCenter(Location):
 class ForumCategory(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now=True)
     icon = models.CharField(max_length=50, default='chat-dots')
     order = models.IntegerField(default=0)
     is_active = models.BooleanField(default=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.name
@@ -102,6 +104,7 @@ class ForumCategory(models.Model):
     class Meta:
         ordering = ['order', 'name']
         verbose_name_plural = 'Forum Categories'
+
 
 class ForumPost(models.Model):
     title = models.CharField(max_length=200)
